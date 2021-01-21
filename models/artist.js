cdaaee0f3e6d232186ce92744afa78e13787a3cd
bryansmithsf <github@bryansmithsf.com>
2021-01-20 19:47:07 -0800

    created Artist and Artwork models

---
+++ models/artist.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+    const Artist = sequelize.define('Artist', {
+      // Giving the Author model a name of type STRING
+      name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              len: [1],
+          }
+      },
+      screenName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1],
+        }
+      },
+      phone: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+      },
+      email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate:{
+            isEmail: true,
+          }
+      },
+      dateCreated: {
+          type: DataTypes.DATE,
+          allowNull: false,
+      }
+
+    });
+  
+    Artist.associate = (models) => {
+      // Associating Author with Posts
+      // When an Author is deleted, also delete any associated Posts
+      Artist.hasMany(models.Artwork, {
+        onDelete: 'cascade',
+      });
+    };
+  
+    return Artist;
+  };
+  
