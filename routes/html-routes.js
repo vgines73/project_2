d4341827510ff36abec5b4760592c0f55f06da64
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2021-01-24 21:51:15 -0800

    fixed edit button.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,21 +5,23 @@ const path = require('path');
 module.exports = (app) => {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
+  // index route loads index.html
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../public/index.html'))
   );
 
-  // cms route loads cms.html
+  // artist route loads artist.html
   app.get('/artist', (req, res) =>
     res.sendFile(path.join(__dirname, '../public/artist.html'))
   );
 
+  // artwork route loads to artwork.html
   app.get('/artwork', (req, res) => 
       res.sendFile(path.join(__dirname, '../public/artwork.html'))
   );
   
+  // create route loads to create.html
   app.get('/create', (req, res) => 
-  res.sendFile(path.join(__dirname, '../public/artwork.html'))
+  res.sendFile(path.join(__dirname, '../public/create.html'))
   );
 };
