724e658cdda27c684c4a7f2581dc66c6afd63164
bryansmithsf <github@bryansmithsf.com>
2021-01-27 18:11:14 -0800

    server will now drop and rebuild db on startup

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ artistRouter(app);
 artworkRouter(app);
 
 // Syncing our sequelize models and then starting our Express app
-db.sequelize.sync({ force: false }).then(() => {
+db.sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`Listening on PORT http://localhost:${PORT}`));
 });
